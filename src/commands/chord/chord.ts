7e6616ee103f0b4f4e5bfb0662ed29bfe6a6cc07
rdroe <d@vidroe.com>
2023-11-28 11:09:41 -0600

    basic, realized graph

--- src/commands/chord/chord.ts
+++ src/commands/chord/chord.ts
@@ -1,36 +1,15 @@
 import { fakeCli } from 'peprn/browser'
-import { Module, awaitAll } from 'peprn/util'
-import { allScales, detectScales } from 'src/lib/graphh'
-import { Chord, Note, Scale, Mode, Collection, Progression } from 'tonal'
+import { Module } from 'peprn/util'
+import { fns, ProgressionGraphNode, allScales, detectScales, makeProgNodeTranslator, minor, noteInversions, optionalRomans, romanChordNameToReal, scaleLetters, combineEntriesByName, ProgressionOptions } from 'src/lib/graphh'
+import { randomInt } from 'src/lib/helpers'
+import { Chord, Note, Scale, Mode, Collection, Progression, RomanNumeral } from 'tonal'
 import { z } from 'zod'
 
-const whereTriad = (scaleNames: string[], aliases: string[]) => {
-    const tonicScaleArr: [tonic: string, scaleName: string][] = z.array(z.string()).transform((strs) => {
-        return strs.map((str) => {
-            const split = str.split(' ')
-            const tonic = split.shift()
-            return [tonic, split.join(' ')] as [tonic: string, scaleName: string]
-
-        })
-    }).parse(scaleNames)
-
-    const triadsWhereInScale = tonicScaleArr.map(([tonic, scaleName]) => {
-        const triads = Mode.triads(scaleName, tonic)
-        return { tonic, scaleName, triads }
-    }).filter(({ triads }) => {
-        const includedAlias = aliases.find((alias) => {
-            return triads.includes(alias)
-        })
-        return includedAlias !== undefined
-    })
-    return triadsWhereInScale
-}
 
 export const chord: Module = {
     fn: async () => {
         return null
     },
-
     submodules: {
         allTriads: {
             fn: async () => {
@@ -42,16 +21,32 @@ export const chord: Module = {
                 return { formatted }
             }
         },
+        translateLetter: {
+            fn: async (args) => {
+                const [userLetter = "", userScale = "", noteLetter = null] = args.positionalNonCommands
+                /*
+                const translateFn = letterToRomanNumeralMap(userLetter, userScale)
+                if (typeof noteLetter !== 'string') throw new Error(`translatable note is required; instead ${noteLetter}`)
+
+                return translateFn(noteLetter)
+*/
+            }
+        },
+
+        fromRoman: {
+            fn: async (args, moduleCalls) => {
+                const [userLetter = "", userScale = "", romanName = null] = args.positionalNonCommands
+                return romanChordNameToReal(userLetter, userScale, romanName)
+            },
+        },
         allProgressions: {
             fn: async () => {
-
                 const allTriadsRaw = await fakeCli('chord allTriads', 'cli')
                 const allTriads = z.array(z.object({
                     scaleTonic: z.string(),
                     triads: z.array(z.string()),
                     scaleType: z.string()
                 })).parse(allTriadsRaw.formatted)
-
                 const formatted = allTriads.map(({ scaleTonic, triads, scaleType }) => {
                     return Progression.toRomanNumerals(scaleTonic, triads).map((romanNum, idx) => ({
                         scaleTonic,
@@ -59,15 +54,82 @@ export const chord: Module = {
                         [romanNum]: triads[idx]
                     }))
                 })
-
                 return { formatted }
             }
         },
+        roman: {
+            fn: async (args) => {
+                const [romanName = null] = args.positionalNonCommands
+                return RomanNumeral.get(romanName)
+            }
+        },
         graph: {
-            fn: async (args, moduleCalls) => { },
+            fn: async () => {
+            },
             submodules: {
+                ltrs: {
+                    fn: async ({ positionalNonCommands }) => {
+                        const [userLetter = "", userScale = ""] = positionalNonCommands
+                        return scaleLetters(userLetter, userScale)
+                    }
+                },
                 test: {
-                    fn: async (args) => { }
+                    fn: async ({ positionalNonCommands }) => {
+
+                        const [userLetter = "", userScale = ""] = positionalNonCommands
+
+                        const names = Object.keys(minor)
+                        const untranslatable = names.map((romanName) => {
+
+                            if (fns[romanName as keyof typeof fns]) { return null }
+                            const translated = romanChordNameToReal(userLetter, userScale, romanName)
+
+
+                            if (!translated) {
+                                return romanName
+                            }
+
+                            return null
+                        }).filter((elem) => elem !== null && !optionalRomans.includes(elem))
+
+                        if (untranslatable.length) {
+                            throw new Error(`Not all roman names were translatable. Make sure this is a minor key. ${JSON.stringify(untranslatable)} ; scale: ${userLetter} ${userScale}`)
+                        }
+
+                        const scaledGraph =
+                            Object.entries(minor).reduce((accum, [romanName, progNodes]) => {
+
+                                const realizedName = fns[romanName as keyof typeof fns]
+                                    ? romanName
+                                    : romanChordNameToReal(userLetter, userScale, romanName)
+
+
+
+                                if (accum.find(([x, _]) => x === realizedName)) {
+                                    console.error(`prog node already translated; ${romanName} in ${userLetter} ${userScale} ${JSON.stringify({ romanName, realizedName, progNodes }, null, 2)}`)
+                                }
+
+                                const realizedOptions = progNodes.map(makeProgNodeTranslator(userLetter, userScale))
+
+
+                                return [...accum, [realizedName, realizedOptions]]
+
+                            }, [] as [romanName: string, progNodes: ProgressionGraphNode][])
+
+                        const combinedScaleGraphEntries = scaledGraph.map(([name, pOpts]: [nm: string, pOpts: ProgressionOptions[]]) => {
+                            return [name, combineEntriesByName(
+                                pOpts)
+
+                            ]
+                        })
+
+
+
+                        return {
+                            formatted: Object.fromEntries(combinedScaleGraphEntries
+                            )
+                        }
+                    }
                 },
                 starters: {
                     fn: async ({ positionalNonCommands }) => {
@@ -112,7 +174,7 @@ export const chord: Module = {
                     fn: async (args, moduleCalls) => {
                         const [chordName] = await moduleCalls['chord $']
                         const notes = Chord.get(chordName)?.notes || []
-                        const notesPermuted = Collection.permutations(notes)
+                        const notesPermuted = noteInversions(chordName)
                         const aliases = [notes, ...notesPermuted].map((notes1) => Chord.detect(notes1)).flat()
                         return { aliases: aliases.concat([chordName]) }
                     },
--- src/lib/graphh.ts
+++ src/lib/graphh.ts
@@ -2,11 +2,13 @@ import { Chord, Note, Mode, Scale, Progression, Collection } from "tonal"
 
 export const noteNames = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', "A", "Bb", "B"]
 const allModes = Mode.all()
+
 export const allScales = allModes.map((m) => {
     const scales = noteNames.map(nn => `${nn} ${m.name}`)
     const scaleObjs = scales.map(scName => Scale.get(scName))
     return scaleObjs
 }).flat()
+
 const inScale = (notes: string[], scale: { notes: string[] }) => {
     const indexMap = notes.reduce((accum, curr) => {
         const idx = scale.notes.indexOf(curr)
@@ -51,10 +53,11 @@ export const detectAllScales = (notes: string[]) => {
 export type ChordNameWithNotes = {
     name: string
     notes: string[]
-
 }
 
-
+type EnabledChordNameWithNotes = ChordNameWithNotes & {
+    enabler: string[] | null
+}
 export const N6 = function N6(tonic: string, scaleName: string) {
     const secondDegree = Scale.degrees(`${tonic} ${scaleName}`)(2)
     const neoRoot = Note.simplify(`${secondDegree}b`.replace('#b', ''))
@@ -89,135 +92,291 @@ Think of your key as C. The formula for the chord is (using scale degrees) b6, 1
     return [flatSix, one, sharpFour]
 }
 
+export const fns = {
+    V64,
+    Aug6,
+    N6
+}
 
-type ProgressionGraphNode = {
+export type ProgressionGraphNode = {
     name: string,
-    prev?: string[],
     next: string[] | "Any",
     dotted?: string[]
+    prev?: string[],
 }
 
-export type PlayedAs = {
-    chordWithNotes: ChordNameWithNotes
+export const scaleLetters = (scaleTonic: string, scaleName: string) => {
+    const ten = oneIndexedArr(10).map(Scale.degrees(`${scaleTonic} ${scaleName}`))
+    const len = Scale.get(`${scaleTonic} ${scaleName}`).notes.length
+    if (len !== 7) throw new Error(`Cannot get progressions for ${len} sized scale`)
+    return ten.slice(0, len)
+
+}
+
+export const romanToLetterEntries = (scaleTonic: string, scaleName: string) => {
+    const letters = scaleLetters(scaleTonic, scaleName)
+    const romans = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+    const entries1 = letters.map((ltr, idx) => {
+        return [romans[idx], ltr]
+    })
+
+    const entries2 = letters.map((ltr, idx) => {
+        return [romans[idx], Note.enharmonic(ltr)]
+    })
+
+    const all1 = [...entries1, ...entries2]
+
+    all1.sort(
+        ([lA], [lB]) => lB.length - lA.length)
+    const flatAndSharpRomans = all1.filter(([ltr]) => {
+        return ltr.includes('b') || ltr.includes('#')
+    }).map(([flatOrSharpLetter, roman]) => {
+        const acc = Note.get(flatOrSharpLetter).acc
+        return [`${acc}${roman}`, flatOrSharpLetter]
+    })
+    return all1.concat(flatAndSharpRomans)
+
+}
+
+
+const romanChordNameToReal_ = (scaleTonic: string, scaleName: string, romanName: string) => {
+
+    const entries = romanToLetterEntries(scaleTonic, scaleName)
+    const newName = entries.reduce((accum, curr) => {
+        return accum.replace(curr[0], curr[1])
+    }, romanName)
+    if (newName === romanName) return ""
+    return newName
+}
+
+
+export const romanChordNameToReal = (scaleTonic: string, scaleName: string, romanName: string,): string => {
+    if (romanName.includes('/')) {
+        const [main, bass] = romanName.split('/')
+        const mainChordName = romanChordNameToReal_(scaleTonic, scaleName, main)
+        const bassChordName = romanChordNameToReal_(scaleTonic, scaleName, bass)
+        if (!mainChordName || !bassChordName) return ""
+        return `${mainChordName}/${bassChordName}`
+    }
+    return romanChordNameToReal_(scaleTonic, scaleName, romanName)
+}
+
+export const romanizeSlashChord = (tonic: string, slashRoman: string) => {
+    const slashed = slashRoman.split('/')
+    const [name1, name2] = Progression.fromRomanNumerals(tonic, slashed)
+    if (!name1 || !name2) return ''
+    return `${name1}/${name2}`
+}
+
+
+
+export const optionalRomans = ["IImdim"]
+export function makeProgNodeTranslator(
+    userLetter: string,
+    userScale: string
+): (progNode: ProgressionGraphNode) => ProgressionOptions | null {
+
+    const notesAreInScale = (notes: string[]) => {
+        const strippedNotes = notes.map(nt => Note.get(nt).pc)
+        const scaleNames = Scale.detect(strippedNotes, { tonic: userLetter })
+
+        return scaleNames.includes(`${userLetter} ${userScale}`)
+    }
+
+    return (progNodeIn) => {
+        let translatedSource: ChordNameWithNotes | undefined
+
+        if (fns[progNodeIn.name as keyof typeof fns]) {
+            translatedSource = {
+                name: progNodeIn.name,
+                notes: fns[progNodeIn.name as keyof typeof fns](userLetter, userScale)
+            }
+        } else {
+            const newName = romanChordNameToReal(userLetter, userScale, progNodeIn.name)
+            translatedSource =
+                chordNameWithNotes(newName)
+        }
+
+        if (!translatedSource) {
+            console.error(`Could not translate ${progNodeIn.name} in ${userLetter} ${userScale}`)
+            return null
+        }
+
+        const next = progNodeIn.next === "Any" ? "Any" : progNodeIn.next.map((romanName) => {
+            const realizedName = romanChordNameToReal(userLetter, userScale, romanName)
+            const initCnwn = chordNameWithNotes(realizedName)
+            //            const cnwn = initCnwn && notesAreInScale(initCnwn.notes) ? initCnwn : null
+            const cnwn = initCnwn
+            if (cnwn === null || cnwn.notes.length === 0) return null
+
+
+            return {
+                ...cnwn,
+                enabler: progNodeIn.prev ? progNodeIn.prev.map((romanName: string) => {
+                    return romanChordNameToReal(userLetter, userScale, romanName)
+                }) : null
+            }
+        }).filter(n => n !== null)
+
+        const dotted = progNodeIn?.dotted?.map((romanName) => {
+            const realizedName = romanChordNameToReal(userLetter, userScale, romanName)
+            const cnwn = chordNameWithNotes(realizedName)
+            if (cnwn === null || cnwn.notes.length === 0) return null
+            return {
+                ...cnwn,
+                enabler: progNodeIn.prev ? progNodeIn.prev.map((romanName: string) => {
+                    return romanChordNameToReal(userLetter, userScale, romanName)
+                }) : null
+            }
+        }).filter(n => n !== null)
+
+        return {
+            roman: progNodeIn.name,
+            scaleName: userScale,
+
+            scaleTonic: userLetter,
+            translatedSource,
+            next,
+            dotted: dotted ?? []
+        }
+    }
+}
+
+export type ProgressionOptions = {
+    roman: string
     scaleTonic: string
     scaleName: string
-    fromRomanized: string
-    unusedContemporaries: PlayedAs[]
-    previous: PlayedAs | null
+    translatedSource: ChordNameWithNotes
+    next: EnabledChordNameWithNotes[]
+    dotted: EnabledChordNameWithNotes[]
 }
 
-export const minor: ProgressionGraphNode[] = [
+export const combineEntriesByName = (progOptions: ProgressionOptions[]): ProgressionOptions | null => {
+
+    if (progOptions.length === 0) return null
+    if (progOptions.length === 1) return progOptions[0]
+    return progOptions.reduce((accum, curr, idx) => {
+        if (idx === 0) return accum
+        return {
+            ...accum,
+            next: [...accum.next, ...curr.next],
+            dotted: [...accum.dotted, ...curr.dotted]
+        }
+    }, progOptions[0] as ProgressionOptions)
+}
+
+export const minor: { [name: string]: ProgressionGraphNode[] } = {
+    'IVm': [{
+        name: 'IVm',
+        next: ["VII"],
+        dotted: ["VIIdim/III", "V7/III"]
+    },
+    // double-box top
     {
         name: "IVm",
-        next: ["VII"],
-        dotted: ["VIImdim/VI", "V7/III"]
-    },
-    {
-        name: "VII",
+        prev: ["VI", "VIIdim/VIm", "V7/VIm"],
+        next: [
+            "VIIdim/V", "V/V", // small upper fork
+            "V64", "VIIdim", "V"
+        ], // big confusing box
+    }],
+    'VII': [{
+        name: 'VII',
         next: ["III"],
         dotted: ["I"],
-    },
-    {
+    }],
+    'III': [{
         name: "III",
-        next: ["VIImdim/VI", "V7/VI", "VI",],
-    },
-    {
+        next: ["VIIdim/VI", "V7/VI", "VI",],
+    }],
+    'VI': [{
         name: "VI",
-        next: ["IImdim", "IVm", /* N6 Aug6 */],
-
-    },
-    // double-box top                                                                                        
-    {
-        name: "IVm",
-        prev: ["VI"],
-        next: [
-            "VIImdim/V", "V/V", // small upper fork                                                          
-            "V64", "VIImdim", "V"], // big confusing box                                                     
-    },
+        next: ["IIdim", "IVm", /* N6 Aug6 */],
+    }],
     // double-box bottom                      
-    {
-        name: "IImdim",
+    'IIdim': [{
+        name: "IIdim",
         prev: ["VI"],
         next: [
-            "VIImdim/V", "V/V", // small upper fork                                                         
-            "V64", "VIImdim", "V"],// big confusing box                                                     
-    },
+            "VIIdim/V", "V/V", // small upper fork                                                         
+            "V64", "VIIdim", "V"],// big confusing box                                                     
+    }],
     // big confusing box v64
-    {
+    'V64': [{
         name: "V64",
         dotted: ["I"],
         next: ["N6", "Aug6", "I"],
-    },
+    }],
     // big confusing box viio (must play V before leaving ???)                                     
-    {
-        name: "VIImdim",
-        prev: ["IVm", "IImdim"],
+    'VIIdim': [{
+        name: "VIIdim",
+        prev: ["IVm", "IIdim"],
         next: ["V"],
-    },
+    }],
     // big confusing box V                                     
-    {
+    'V': [{
         name: "V",
         next: ["V64", "Aug6"],
         dotted: ["I"],
-    },
+    }],
     // non-box with sixes N6   
-    {
+    'N6': [{
         name: "N6",
         next: ["V"],
-    },
+    }],
     // non-box with sixes Aug6
-    {
+    'Aug6': [{
         name: "Aug6",
         next: ["V"],
-    },
-    // upper boxesssssss ltr                                                                                
-    // 1                                                                                 
-    {
-        name: "VIImdim/IV",
-        next: ["VIImdim/VII", "V7/VII"],
-    },
-    {
+    }],
+    // upper boxesssssss ltr                      
+    // 1    
+    'VIIdim/IV': [{
+        name: "VIIdim/IV",
+        next: ["VIIdim/VII", "V7/VII"],
+    }],
+    'V7/IV': [{
         name: "V7/IV",
-        next: ["VIImdim/VII", "V7/VII"],
-    },
+        next: ["VIIdim/VII", "V7/VII"],
+    }],
     // 2
-    {
-        name: "VIImdim/VII",
-        next: ["VIImdim/III", "V7/III", /*downward arrow*/ "VII"],
-    },
-    {
+    'VIIdim/VII': [{
+        name: "VIIdim/VII",
+        next: ["VIIdim/III", "V7/III", /*downward arrow*/ "VII"],
+    }],
+    'V7/VII': [{
         name: "V7/VII",
-        next: ["VIImdim/III", "V7/III", /*downward arrow*/ "VII"],
-    },
+        next: ["VIIdim/III", "V7/III", /*downward arrow*/ "VII"],
+    }],
     // 3                 
-    {
-        name: "VIImdim/III",
-        next: ["VIImdim/VIm", "V7/VIm", /*downward arrow*/ "III"],
-    },
-    {
+    'VIIdim/III': [{
+        name: "VIIdim/III",
+        next: ["VIIdim/VIm", "V7/VIm", /*downward arrow*/ "III"],
+    }],
+    'V7/III': [{
         name: "V7/III",
-        next: ["VIImdim/VIm", "V7/VIm", /*downward arrow*/ "III"],
-    },
+        next: ["VIIdim/VIm", "V7/VIm", /*downward arrow*/ "III"],
+    }],
     // 4                   
-    {
-        name: "VIImdim/VIm",
-        next: ["IVm", "IImdim", /*downward arrow */ "VI"],
-    },
-    {
+    'VIIdim/VIm': [{
+        name: "VIIdim/VIm",
+        next: ["IVm", "IIdim", /*downward arrow */ "VI"],
+    }],
+    'V7/VIm': [{
         name: "V7/VIm",
-        next: ["IVm", "IImdim", /*downward arrow */ "VI"],
-    },
+        next: ["IVm", "IIdim", /*downward arrow */ "VI"],
+    }],
     // 5                   
-    {
-        name: "VIImdim/V",
-        next: ["V64", "VIImdim"],
-    },
-    {
+    'VIIdim/V': [{
+        name: "VIIdim/V",
+        next: ["V64", "VIIdim"],
+    }],
+    'V/V': [{
         name: "V/V",
-        next: ["V64", "VIImdim"],
-    },
-    { name: "Im", next: "Any" },
-]
+        next: ["V64", "VIIdim"],
+    }],
+    'Im': [{ name: "Im", next: "Any" }],
+}
 
 const oneIndexedArr = (len: number) => {
     if (len <= 0) return []
@@ -256,14 +415,17 @@ export const noteInversions = (chordName: string, userOct: number = 3): string[]
     }
 
     const validTonic = `${letter}${oct}`
-    const noteCount = Chord.getChord(chordType, validTonic)?.notes?.length
+    const notesArr = Chord.getChord(chordType, validTonic)?.notes
+    const noteCount = notesArr.length
     if (typeof noteCount !== "number") return []
     const mappableNums = oneIndexedArr(noteCount)
     const rotatedIndexes = rotations(mappableNums)
     const degreesFn = Chord.degrees([validTonic, chordType]);
-    return rotatedIndexes.map((orderedIndexes) => {
+    const rots = rotatedIndexes.map((orderedIndexes) => {
         return orderedIndexes.map(degreesFn)
     })
+
+    return [notesArr, ...rots]
 }
 
 export const nakedNoteInversions = (cn: string, oct: number = 3) => {
@@ -271,6 +433,7 @@ export const nakedNoteInversions = (cn: string, oct: number = 3) => {
 }
 
 export const detectScales = (notes: string[], userLetter?: string, userScale?: string) => {
+
     const allScales = detectAllScales(notes)
 
     if (!userLetter || !userScale) {
@@ -294,23 +457,42 @@ export const detectScales = (notes: string[], userLetter?: string, userScale?: s
     return scales
 }
 
-export const chordNameWithNotes = (chordName: string, oct: number = 3): ChordNameWithNotes => {
+export const fnChordNameWithNotes = (fnName: keyof typeof fns, tonic: string, scaleName: string) => {
+    return {
+        name: fnName,
+        notes: fns[fnName](tonic, scaleName)
+    }
+}
+
+export const chordNameWithNotes = (chordName: string, oct: number = 3): ChordNameWithNotes | null => {
+
+
+
 
     if (chordName.includes('/')) {
+
         const [main, bass] = chordName.split('/')
         if (!main || !bass) return null
         const mainInversions = noteInversions(main, oct)
-        const bassNames = Chord.get(bass)?.notes || []
-        const bassNote = bassNames[0]
+        const bassInversions = nakedNoteInversions(bass, oct)
 
-        const bassLetter = Note.get(bassNote).letter
-        console.log('notes in cnwn fn', { mainInversions, bassLetter })
-        const permutedNotes = mainInversions.find((notes) => {
-            return notes[0].startsWith(bassLetter) || Note.enharmonic(notes[0]).startsWith(bassLetter)
-        })
+        // dont stop trying at the first available bass note.
+        // search through more possibilities
+        const permutedNotes = bassInversions.reduce((nullOrAnswer, [currBassNote]: string[]) => {
+            if (nullOrAnswer) return nullOrAnswer
+            return mainInversions.find(
+                ([firstNote]) => {
+
+                    return firstNote.startsWith(currBassNote) || firstNote.startsWith(Note.enharmonic(currBassNote))
+
+                }) ?? null
+
+        }, null as null | string[])
+
 
         if (!permutedNotes) {
-            throw new Error(`Could not ad-hoc provide the slash chord ${chordName}`)
+            console.error(`Could not ad - hoc provide the slash chord ${chordName}; main chord notes were ${mainInversions[0]} ; bass chord notes were ${bassInversions[0]}`)
+            return null
         }
         return {
             name: chordName,
