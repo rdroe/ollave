8f4628fc05fbe059540d0afef00494586ea68beb
rdroe <d@vidroe.com>
2025-09-01 14:53:38 -0500

    cleanup

--- src/commands/song/observables.ts
+++ src/commands/song/observables.ts
@@ -26,16 +26,9 @@ export const getSongCursor = (tick: number) => {
 }
 
 export const startCueObservable = (startAt?: number) => {
-
     // make a new observable that subscribes to master ticks
     // if the fed-in tick modular-divides to 0 on bar ticks, trigger.
-
-    console.log('startCueObservable', {
-        startAt,
-        song: mem()?.song
-    })
     const song = mem()?.song?.name
-
     if (!song) {
         console.error(mem())
         throw new Error(`Song not initialized`)
@@ -75,7 +68,7 @@ export const startCueObservable = (startAt?: number) => {
             updateExportableTick()
         }
     })
-    console.log('setting up functions for song', song)
+
     Object.entries(mem().functions[song] || {}).forEach(([fnName, fn]) => {
         mem().observables[song][fnName] = songObservable.subscribe({
             next: ({ tick }) => {
@@ -93,50 +86,28 @@ export const stopCueObservable = () => {
     const songName = mem().song.name
     const publishedCursro = mem().adjustedCursor
 
-    console.log('stopping cue observable for song', songName, mem().observables)
-
     const observable =
         mem().observables[songName]
 
     if (observable) {
         mem().songPauses[songName] = barsAtMidi(publishedCursro)[0]
-        console.log('setting song pause for song', songName, 'to', mem().songPauses[songName])
         Object.entries(mem().observables[songName] || {}).forEach(([fnName,observable]) => {
-            console.log('unsubscribing from function', fnName, 'for song', songName)
             observable.unsubscribe()
         })
     } else {
         console.error('no observable found for song', songName)
     }
-    console.log(JSON.parse(JSON.stringify({
-        'mem().observables[songName]': mem().observables,
-        'mem().songPauses[songName]': mem().songPauses,
-        'mem().functions[songName]': mem().functions,
-    })))
 
     Object.entries(mem().functions[songName] || {}).forEach(([fnName, fn]) => {
-        console.log('unsubscribing from function', fnName, 'for song', songName)
         mem().observables[songName][fnName]?.unsubscribe()
     })
 
 }
 
 export const deleteCueObservable = (songName: string) => {
-
-    console.log('deleting cue observable for song',songName,'before', songName, JSON.stringify({
-        'mem().observables[songName]': mem().observables,
-        'mem().songPauses[songName]': mem().songPauses,
-        'mem().functions[songName]': mem().functions,
-    }) , 2) 
     const observables: { [songName: string]: { [fnName: string]: Subscription } } = Object.fromEntries(Object.entries(mem().observables).filter(([key]) => key !== songName)) 
     const songPauses: { [songName: string]: BarTagPercent } = Object.fromEntries(Object.entries(mem().songPauses).filter(([key]) => key !== songName))
     const functions: { [songName: string]: { [fnName: string]: (tick: number, rawTick: number, snapShot: Mem, songName: string) => void } } = Object.fromEntries(Object.entries(mem().functions).filter(([key]) => key !== songName))
-    console.log('deleting cue observable for song', songName, JSON.stringify({
-        'mem().observables[songName]': mem().observables,
-        'mem().songPauses[songName]': mem().songPauses,
-        'mem().functions[songName]': mem().functions,
-    }) , 2)
-
     mem().songPauses = songPauses 
     mem().observables = observables 
     mem().functions = functions 
